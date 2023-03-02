01641dfb01ca4cfe43c051f03a581968cc65a74b
ania221B <abuwaj@gmail.com>
2023-03-02 23:51:00 +0100

    button functionality added

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const adviceGenerator = document.querySelector('.advice-generator')
+const advice = adviceGenerator.querySelector('.advice')
+
+function getAdvice () {
+  fetch('https://api.adviceslip.com/advice')
+    .then(response => response.json())
+    .then(response => {
+      const slip = response.slip
+      const string = `<h2 class="title">Advice #${slip.id}</h2>
+        <blockquote>
+              <p>${slip.advice}</p>
+            </blockquote>`
+      advice.innerHTML = string
+    })
+}
+adviceGenerator.addEventListener('click', e => {
+  if (!e.target.closest('.button')) return
+  getAdvice()
+})
