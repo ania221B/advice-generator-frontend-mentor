4ef371a664791a0fc0589e234cdae03f94de7fe7
ania221B <abuwaj@gmail.com>
2023-03-07 21:00:49 +0100

    error handling added

--- js/script.js
+++ js/script.js
@@ -5,13 +5,35 @@ function getAdvice () {
   fetch('https://api.adviceslip.com/advice', {
     cache: 'reload'
   })
-    .then(response => response.json())
     .then(response => {
-      const slip = response.slip
-      const string = `<h2 class="title">Advice #${slip.id}</h2>
+      if (response.ok) {
+        return response.json()
+      } else {
+        console.log(response)
+        return Promise.reject(response.statusText)
+      }
+    })
+    .then(body => {
+      if (!body.slip) {
+        const message = body.message
+        const string = `<h2 class="title">${message.type}</h2>
+        <img src="images/error-icon.svg" alt="">        
+        <p>${message.text}</p>`
+        advice.innerHTML = string
+      } else {
+        const slip = body.slip
+        const string = `<h2 class="title">Advice #${slip.id}</h2>
         <blockquote>
               <p>${slip.advice}</p>
             </blockquote>`
+        advice.innerHTML = string
+      }
+    })
+    .catch(_ => {
+      const string = `<h2 class="title">Connection Error</h2>
+        <img src="images/error-icon.svg" alt="">        
+          <p>Service is unreachable. Please check network connection.</p>
+         `
       advice.innerHTML = string
     })
 }
