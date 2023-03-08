eb5a7a49efe87354aa268090fd499ddc24c8ff9e
ania221B <abuwaj@gmail.com>
2023-03-08 23:04:21 +0100

    sanitizer used+error handling updated

--- js/script.js
+++ js/script.js
@@ -9,32 +9,45 @@ function getAdvice () {
       if (response.ok) {
         return response.json()
       } else {
-        console.log(response)
-        return Promise.reject(response.statusText)
+        return Promise.reject(response)
       }
     })
     .then(body => {
       if (!body.slip) {
         const message = body.message
-        const string = `<h2 class="title">${message.type}</h2>
-        <img src="images/error-icon.svg" alt="">        
+        const string = `
+        <img src="images/error-icon.svg" alt="">
+        <h2 class="title error-title">${message.type}</h2>    
         <p>${message.text}</p>`
-        advice.innerHTML = string
+
+        advice.innerHTML = DOMPurify.sanitize(string)
       } else {
         const slip = body.slip
         const string = `<h2 class="title">Advice #${slip.id}</h2>
         <blockquote>
               <p>${slip.advice}</p>
             </blockquote>`
-        advice.innerHTML = string
+
+        advice.innerHTML = DOMPurify.sanitize(string)
       }
     })
-    .catch(_ => {
-      const string = `<h2 class="title">Connection Error</h2>
-        <img src="images/error-icon.svg" alt="">        
-          <p>Service is unreachable. Please check network connection.</p>
-         `
-      advice.innerHTML = string
+    .catch(error => {
+      const string = !error.body
+        ? `
+      <img src="images/error-icon.svg" alt="">
+      <h2 class="title error-title">Network Error</h2>
+      <p>Please check network connection or contact support.</p>
+      `
+        : `
+      <img src="images/error-icon.svg" alt="">
+      <header>
+      <h2 class="title error-title">Error</h2>
+      <p class="error-subtitle">Code: ${error.status}</p>
+      <p class="error-subtitle">Message: ${error.statusText}</p>  
+      </header>     
+      <p>Please check network connection or contact support.</p>
+      `
+      advice.innerHTML = DOMPurify.sanitize(string)
     })
 }
 adviceGenerator.addEventListener('click', e => {
